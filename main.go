54919fa6897e2792b8918972890f6322159756f9
kaznishi <kaz.nishimu@gmail.com>
2018-07-24 21:34:50 +0900

    LT資料中で説明しやすいように実装を変更

--- main.go
+++ main.go
@@ -9,57 +9,52 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
-
 const (
 	namespace = "abcd"
 )
 
-type myCollector struct {
-	exampleCount prometheus.Counter
-	exampleGauge prometheus.Gauge
+type myCollector struct{}
+
+var (
+	exampleCount = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: namespace,
+		Name:      "example_count",
+		Help:      "example counter help",
+	})
+	exampleGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: namespace,
+		Name:      "example_gauge",
+		Help:      "example gauge help",
+	})
+)
+
+func (c myCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- exampleCount.Desc()
+	ch <- exampleGauge.Desc()
 }
 
-func newMyCollector() *myCollector {
-	return &myCollector{
-		exampleCount: prometheus.NewCounter(prometheus.CounterOpts{
-			Namespace: namespace,
-			Name:      "example_count",
-			Help:      "example counter help",
-		}),
-		exampleGauge: prometheus.NewGauge(prometheus.GaugeOpts{
-			Namespace: namespace,
-			Name:      "example_gauge",
-			Help:      "example gauge help",
-		}),
-	}
-}
-
-func (c *myCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- c.exampleCount.Desc()
-	ch <- c.exampleGauge.Desc()
-}
-
-func (c *myCollector) Collect(ch chan<- prometheus.Metric) {
+func (c myCollector) Collect(ch chan<- prometheus.Metric) {
 	dummyStaticNumber := float64(1234)
 
 	ch <- prometheus.MustNewConstMetric(
-		c.exampleCount.Desc(),
+		exampleCount.Desc(),
 		prometheus.CounterValue,
 		float64(dummyStaticNumber),
 	)
 	ch <- prometheus.MustNewConstMetric(
-		c.exampleGauge.Desc(),
+		exampleGauge.Desc(),
 		prometheus.GaugeValue,
 		float64(dummyStaticNumber),
 	)
 }
 
+var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
+
 func main() {
 	flag.Parse()
 
-	myCollector := newMyCollector()
-	prometheus.MustRegister(myCollector)
+	var c myCollector
+	prometheus.MustRegister(c)
 
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(*addr, nil))
